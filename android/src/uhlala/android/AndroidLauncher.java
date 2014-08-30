b62fecd94c0763f362759cd04c7ee0b92ab7d589
Felipe Lindemberg <you@example.com>
2014-08-30 16:09:34 -0300

    kick off gdf

---
+++ android/src/uhlala/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package uhlala.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import uhlala.uhlalaMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new uhlalaMain(), config);
+	}
+}
